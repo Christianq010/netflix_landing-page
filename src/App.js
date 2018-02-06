de3175ad679a9b2edba0ebf28f89ff4625effcef
Christiaan Quyn <christianq010@gmail.com>
2018-02-06 23:29:29 +0530

    Styling: 3 Features with flexbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Header from './components/Header/Header'
+import Features from './components/Features/Features'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Features />
       </div>
     );
   }
