d46349e8b702304c018fa1c5c4cb82995c9e49b3
Christiaan Quyn <christianq010@gmail.com>
2018-02-06 14:24:02 +0530

    Styling: Header and Nav bar - Add bg img, Logo and sign in button

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import styled from 'styled-components';
+import Nav from './../Nav/Nav';
+import classes from './styles.module.css';
+
+const header = () => {
+    return (
+      <header className="App-header">
+        <Nav />
+        <h1 className={classes.default}>See whats next</h1>
+        <p>Some random content</p>
+      </header>
+    )
+}
+
+export default header;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import styled, { css } from styled-components;
+import netflixlogo from './img/logo.svg';
+import './styles.module.css';
+
+const Button = styled.button`
+  color: white;
+  background-color: #e50914;
+  line-height: normal;
+  margin: 18px 3% 0 0;
+  padding: 7px 17px;
+  font-weight: 400;
+  border-radius: 3px;
+  font-size: 16px;
+  text-decoration: one;
+  
+  ${props => props.right && css`
+    float: right;
+  `}
+`;
+
+const nav = () => {
+    return (
+      <nav>
+        <a href={"www.google.lk"} className="logo">
+            <img src={netflixlogo} alt="Netflix Logo" />
+        </a>
+        <Button right>Sign In</Button>
+      </nav>
+    )
+}
+
+export default nav;
