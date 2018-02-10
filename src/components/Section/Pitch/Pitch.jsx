562a5bbcb0f412c37acda1e2176f9a469eee5f53
Christiaan Quyn <christianq010@gmail.com>
2018-02-10 18:18:43 +0530

    Refactor: Section / Pitch - Use Styled components instead of scss file (h2, p and btn)

--- src/components/Section/Pitch/Pitch.jsx
+++ src/components/Section/Pitch/Pitch.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 
-import './styles.module.scss';
+
+import styled from 'styled-components';
+
+const Section = styled.section`
+    color: white;
+    margin: 0 3%;
+    position: absolute;
+    top: 35%;
+    font-size: 1.8vw;
+`;
+
+const Title = styled.h1`
+    font-size: 3em;
+    margin: 0 0 0.2em;
+    font-weight: 700;
+`;
+
+const Subtitle = styled.p`
+    margin: 0 0 0.5em;
+    font-weight: 300;
+`;
+
+const MainJoinButton = styled.button`
+    font-size: 14px;
+    letter-spacing: 1.9px;
+    font-weight: 200;
+    margin: 0.5em 0.5em 0.5em 0;
+    padding: 12px 2em;
+    color: white;
+    background-color: #e50914;
+    cursor: pointer;
+    text-decoration: none;
+    vertical-align: middle;
+    font-family: Arial, sans-serif;
+    border-radius: 2px;
+    user-select: none;
+    text-align: center;
+    border: 0;
+
+    &:hover {
+      background-color: #E53935;
+    }
+`;
 
 const pitch = () => {
     return (
-      <section>
-        <h1 className="pitchTitle">See what's next.</h1>
-        <p className="pitchSubtitle">WATCH ANYWHERE. CANCEL ANYTIME.</p>
-        <button className="joinButton">JOIN FREE FOR A MONTH</button>
-      </section>
+      <Section>
+        <Title>See what's next.</Title>
+        <Subtitle>WATCH ANYWHERE. CANCEL ANYTIME.</Subtitle>
+          <MainJoinButton>JOIN FREE FOR A MONTH</MainJoinButton>
+      </Section>
     )
 }
 
